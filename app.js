610018f985e626bd6427cf06cb8e428f3c4c808a
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-01-13 16:55:37 +0300

    src

--- app.js
+++ app.js
@@ -3,36 +3,20 @@ const fs = require('fs');
 const multer = require('multer')
 const { PythonShell } = require('python-shell');
 // const pyshell = new PythonShell(__dirname + '/python/Sber_excel.py');
+const {uploadFile, upload} = require('./controllers/excel-controller')
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-const upload = multer({ storage: storage })
-
-
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.post('/post', upload.single('file'), (req, res) => {
-  // pyshell.send(req.file);
-const params = {
-  path: req.file.path
-}
+app.post('/post', upload.single('file'), uploadFile)
 
-  PythonShell.run('./python/Sber_excel.py', params, function (err, res) {
-    if (err) throw err;
-    console.log(res);
-});
+  // pyshell.send(req.file);
+
 //   pyshell.on('message', function (message) {
 //     // received a message sent from the Python script (a simple "print" statement)
 //     console.log(message);
@@ -45,7 +29,7 @@ const params = {
 //     console.log('The exit signal was: ' + signal);
 //     console.log('finished');
 //   });
-})
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
---
+++ controllers/excel-controller.js
@@ -0,0 +1,41 @@
+const multer = require('multer')
+const { parse } = require('../services/excel-service')
+var fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.xlsx')
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+const uploadFile = async (req, res) => {
+  const params = {
+    path: req.file.path
+  }
+
+    PythonShell.run('./python/Sber_excel.py', params, function (err, res) {
+      if (err) throw err;
+      console.log(res);
+  });
+
+  // `${pythonPath} -m py_compile ${filePath}`;
+  // parse(req.file.path)
+  // fs.unlink(req.file.path, function (err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Файл удалён");
+  //   }
+  // });
+}
+
+module.exports = {
+  uploadFile,
+  upload
+}
