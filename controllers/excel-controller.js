20357cca56e249c1cb7d40304615eace17c616ea
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-01-16 13:08:49 +0300

    js

--- controllers/excel-controller.js
+++ controllers/excel-controller.js
@@ -2,7 +2,9 @@ const multer = require('multer')
 const { parse } = require('../services/excel-service')
 var fs = require("fs");
 const { PythonShell } = require('python-shell');
+const {spawn} = require('child_process')
 // import {PythonShell} from 'python-shell';
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -15,27 +17,41 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+// const childPython = spawn('python', [`${__dirname}/python/Sber_excel.py`])
+
+
 
 const uploadFile = async (req, res) => {
+  console.log(req.file.originalname)
+  const childPython = spawn('python', [`${__dirname}/../python/Sber_excel.py`, `${__dirname}/../uploads/${req.file.originalname}`])
 
-
+  childPython.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+    console.log(data)
+  })
+  childPython.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`)
+  })
+  childPython.on('close', (code) => {
+    console.log(`Child process exited with code: ${code}`)
+  })
 
   console.log('//------------------Res---------------//')
-  console.log(req.file)
-  const options = {
-    // pythonPath: ['C:\Users\MarokkoTV\AppData\Local\Programs\Python\Python310\python.exe'],
-    pythonOptions: ['-u'],
-    scriptPath: './python',
-    // encoding: 'utf8',
-    // mode: 'binary',
-    // args: []
-  }
+
+  // const options = {
+  //   // pythonPath: ['C:\Users\MarokkoTV\AppData\Local\Programs\Python\Python310\python.exe'],
+  //   pythonOptions: ['-u'],
+  //   scriptPath: './python',
+  //   // encoding: 'utf8',
+  //   // mode: 'binary',
+  //   // args: []
+  // }
 
 
-    PythonShell.run('Sber_excel.py', options, function (err, res) {
-      if (err) throw err;
-      console.log(res);
-  });
+  //   PythonShell.run('Sber_excel.py', options, function (err, res) {
+  //     if (err) throw err;
+  //     console.log(res);
+  // });
 
   // `${pythonPath} -m py_compile ${filePath}`;
   // parse(req.file.path)
