4f73a750f5801a6845feccb82dabe0ee3a52530c
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2023-02-06 16:51:36 +0300

    Update Alpha_excel.py
    new columns

--- python/bank_controlles/Alpha_excel.py
+++ python/bank_controlles/Alpha_excel.py
@@ -11,6 +11,7 @@ file_extension = sys.argv[3]
 output_directory = sys.argv[4]
 
 path_to_file = directory + file_name + file_extension
+# path_to_file = r"../Выписки по р. с. и иные документы/Альфабанк/Выписка_40702810129050006373_21.11.2021-21.11.2022.xlsx"
 
 xlsx = pd.ExcelFile(path_to_file)
 lst_names = xlsx.sheet_names
@@ -33,15 +34,14 @@ for string in df.values:
         df.columns = df.iloc[0]
         break
     df.drop(labels=index, inplace=True)
-# Удаление последних столбцов и ненужных
 df = df.drop(columns=df.iloc[:, -2:])
 df.columns = ['Дата', 'Номер документа', 'Дебет', 'Кредит', 'Контрагент', 'ИНН', 'КПП',
               'Счет', 'БИК', 'Наименование банка', 'Назначение платежа']
+
 df = df.drop(columns=[df.columns[1], df.columns[6], df.columns[7]])
 df.dropna(axis=0, how='all', inplace=True)
 df.reset_index(drop=True, inplace=True)
 
-# Поиск заголовка
 index = -1
 for i in df[df.columns[0]].values:
     index += 1
@@ -60,6 +60,9 @@ for i in [1, 2]:
     df[df.columns[i]].fillna(0, inplace=True)
 
 df[[df.columns[1], df.columns[2]]] = df[[df.columns[1], df.columns[2]]].round(2)
+
+df = df[['Дата', 'БИК', 'Наименование банка', 'ИНН', 'Контрагент', 'Дебет', 'Кредит', 'Назначение платежа']]
+
 output_file_path = output_directory + file_name + '.json'
 df.to_json(output_file_path, orient='records', force_ascii=False)
 
