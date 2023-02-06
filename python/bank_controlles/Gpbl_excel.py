11cd66fff9f68a54a8e5f2c53c6a3184746a8c51
MassloveDen <78202134+MassloveDen@users.noreply.github.com>
2023-02-06 18:14:23 +0300

    fix file_extention

--- python/bank_controlles/Gpbl_excel.py
+++ python/bank_controlles/Gpbl_excel.py
@@ -12,7 +12,7 @@ file_name = sys.argv[2]
 file_extension = sys.argv[3]
 output_directory = sys.argv[4]
 
-path_to_file = directory + file_name + file_extension
+path_to_file = directory + file_name +'.'+ file_extension
 
 df = pd.read_excel(path_to_file, sheet_name=0)
 
