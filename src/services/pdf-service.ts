7cc4957d638097e0084095af520f23692427dccf
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2023-02-14 14:02:18 +0300

    fix

--- src/services/pdf-service.ts
+++ src/services/pdf-service.ts
@@ -1,14 +1,17 @@
 import { spawnSync } from "child_process";
 import fs from "fs";
+import path from "path";
 
 const convertPDF = async ({ pathFile }: { pathFile: string }) => {
+  const outputDirectory = path.resolve(__dirname, "../../uploads/");
   try {
     const options = [
       `${__dirname}/../../python/pdf-selenium.py`,
       `${pathFile}`,
+      outputDirectory,
     ];
     const childPython = spawnSync("python", options);
-    return childPython
+    // return childPython
     const res = await JSON.parse(childPython.stdout.toString("utf-8"));
     if (res.status !== "success") {
       throw new Error("Ошибка скрипта python");
